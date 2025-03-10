21dae675dac24361bf838d92d85f35d44fc53b20
David Rodriguez <david@Mac-mini-de-David.local>
2025-03-10 19:28:55 +0100

    First commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+	return (
+		<p>This is the output layout, what happened?</p>
+	);
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Montserrat, Lusitana } from 'next/font/google';
+
+export const inter = Inter( { subsets: ['latin-ext'], weight: ["300","500"] } );
+export const montserrat = Montserrat( { subsets: ['latin-ext'], weight: ["300","500"] } );
+export const lusitana = Lusitana( { subsets: ['latin'], weight: ["400"] } );
+
